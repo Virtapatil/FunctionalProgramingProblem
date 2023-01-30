da64fb406e154fb2edf92896d6c0caefee1b1297
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-30 21:29:25 +0530

    [Virta] Modifing Leap Year

---
+++ FunctinalProgramingProblem/LeapYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctinalProgramingProblem
+{
+    internal class LeapYear
+    {
+        public static void FindLeapyear()
+        {
+            Console.WriteLine("Enter the year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            //int year = int.parse(console.ReadLine());
+            if ((year % 4 == 0 && year % 100!= 0) || year % 400 == 0)
+            {
+            Console.WriteLine("year is Leap year:" + year);
+            }
+            else
+            {
+            Console.WriteLine("year is not Leap year:" + year);
+            }
+        }
+    }
+}
--- FunctinalProgramingProblem/Program.cs
+++ FunctinalProgramingProblem/Program.cs
@@ -4,8 +4,25 @@
     {
         static void Main(string[] args)
         {
-            FlipCoin.FindPercentageOfFlipCoin();
-            Console.WriteLine("Functinal Programing Problems : ");
+            Console.WriteLine("Functinal Programing Problem");
+
+            Console.WriteLine("Please enter below number");
+            Console.WriteLine("1.FlipCoin \n 2.LeapYear \n 11.Exit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    FlipCoin.FindPercentageOfFlipCoin();
+                    break;
+                case 2:
+                     LeapYear.FindLeapyear();
+                    break;  
+                default:
+                    Console.WriteLine("please enter the option from above");
+                break;
+            }
+            Console.ReadLine();
         }
     }
 }
