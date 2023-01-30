b00070067f61454ccfabf4907085610e3e9f5571
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-30 21:57:21 +0530

    [Virta] Modifing Power Of 2

---
+++ FunctinalProgramingProblem/PowerOf2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctinalProgramingProblem
+{
+    internal class PowerOf2
+    {
+        public static void FindPowerOf2()
+        {
+            Console.WriteLine("Enter The number");
+            int n=Convert.ToInt32(Console.ReadLine());
+            for(int i=1; i<=n&&n<=31; i++)
+            {
+                double result = Math.Pow(2,i);
+                Console.WriteLine("{0} power {1} : {2}",2,i,result);
+            }
+        }
+    }
+}
--- FunctinalProgramingProblem/Program.cs
+++ FunctinalProgramingProblem/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Functinal Programing Problem");
 
             Console.WriteLine("Please enter below number");
-            Console.WriteLine("1.FlipCoin \n 2.LeapYear \n 11.Exit");
+            Console.WriteLine("1.FlipCoin \n2.LeapYear \n3.PowerOf2 \n 11.Exit");
             int choice = Convert.ToInt32(Console.ReadLine());
 
             switch (choice)
@@ -17,7 +17,10 @@
                     break;
                 case 2:
                      LeapYear.FindLeapyear();
-                    break;  
+                    break;
+                case 3:
+                    PowerOf2.FindPowerOf2();
+                    break;
                 default:
                     Console.WriteLine("please enter the option from above");
                 break;
