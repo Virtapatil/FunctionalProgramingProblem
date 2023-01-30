3c7e388e18de29f2b0bbe2ad88482715ebd08318
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-30 22:44:12 +0530

    [Virta] Modifing Swapping Two Numbers

--- FunctinalProgramingProblem/Program.cs
+++ FunctinalProgramingProblem/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Functinal Programing Problem");
 
             Console.WriteLine("Please enter below number");
-            Console.WriteLine("1.FlipCoin \n2.LeapYear \n3.PowerOf2 \n5.QuotientAndRemainder \n 11.Exit");
+            Console.WriteLine("1.FlipCoin \n2.LeapYear \n3.PowerOf2 \n5.QuotientAndRemainder \n6.SwapTwoNumber \n 11.Exit");
             int choice = Convert.ToInt32(Console.ReadLine());
 
             switch (choice)
@@ -21,8 +21,11 @@
                 case 3:
                     PowerOf2.FindPowerOf2();
                     break;
-                case 4:
+                case 5:
                     QuotientAndRemainder.FindQuotientAndRemainder();
+                    break;
+                case 6:
+                    SwapTwoNumbers.FindSwapTwoNumbers();
                     break;
                 default:
                     Console.WriteLine("please enter the option from above");
---
+++ FunctinalProgramingProblem/SwapTwoNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctinalProgramingProblem
+{
+    internal class SwapTwoNumbers
+    {
+        public static void FindSwapTwoNumbers() 
+        { 
+            int temp = 0;
+            Console.WriteLine("Enter the first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before swapping {0} {1}",a,b);
+            temp = a;
+            a=b;
+            b=temp;
+            Console.WriteLine("After swapping {0} {1}", a, b);
+        }
+    }
+}
